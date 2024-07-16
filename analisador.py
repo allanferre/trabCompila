56282868c2fd1b3198d50059ba5cf890e00b92c4
gabrielguglik <gabrielguglielmik@gmail.com>
2024-07-16 13:53:40 -0300

    tabela definitiva e primeiro  exemplo professor passando

--- analisador.py
+++ analisador.py
@@ -3,20 +3,25 @@ parse_table = {
     'MAIN': {'def': ['FLIST'], 'int': ['STMT'], 'id': ['STMT'], 'print': ['STMT'], 'return': ['STMT'], 'if': ['STMT'], '{': ['STMT'], ';': ['STMT'], '$': []},
     'FLIST': {'def': ['FDEF', 'FLIST'], '$': []},
     'FDEF': {'def': ['def', 'id', '(', 'PARLIST', ')', '{', 'STMTLIST', '}']},
-    'PARLIST': {'int': ['int', 'id', 'PARLIST'], ',': [',', 'PARLIST'], ')': [], '$': []},
-    'STMT': {'int': ['int', 'id', ';'], 'id': ['ATRIBST', ';'], 'print': ['PRINTST', ';'], 'return': ['RETURNST', ';'], 'if': ['IFSTMT'], '{': ['{', 'STMTLIST', '}'], ';': [';']},
-    'ATRIBST': {'id': ['id', '=', 'EXPR']},
+    'PARLIST': {'int': ['int', 'id', 'PARLISTTAIL'], ')': [], '$': []},
+    'PARLISTTAIL': {',': [',', 'int', 'id', 'PARLISTTAIL'], ')': []},
+    'STMT': {'int': ['int', 'id', ';'], 'id': ['ATRIBST'], 'print': ['PRINTST', ';'], 'return': ['RETURNST', ';'], 'if': ['IFSTMT'], '{': ['{', 'STMTLIST', '}'], ';': [';']},
+    'ATRIBST': {'id': ['id', '=', 'EXPR', ';']},
     'FCALL': {'id': ['id', '(', 'PARLISTCALL', ')']},
-    'PARLISTCALL': {'id': ['id', ',', 'PARLISTCALL'], ')': [], '$': []},
+    'PARLISTCALL': {'id': ['id', 'PARLISTCALLTAIL'], ')': [], '$': []},
+    'PARLISTCALLTAIL': {',': [',', 'id', 'PARLISTCALLTAIL'], ')': []},
     'PRINTST': {'print': ['print', 'EXPR']},
-    'RETURNST': {'return': ['return']},
+    'RETURNST': {'return': ['return', 'EXPR'], ';': ['return', ';']},
     'IFSTMT': {'if': ['if', '(', 'EXPR', ')', 'STMT', 'ELSEPART']},
-    'ELSEPART': {'else': ['else', 'STMT'], '': [], '$': []}, # verificar com o professor
+    'ELSEPART': {'else': ['else', 'STMT'], '': [], '$': []},
     'STMTLIST': {'int': ['STMT', 'STMTLIST'], 'id': ['STMT', 'STMTLIST'], 'print': ['STMT', 'STMTLIST'], 'return': ['STMT', 'STMTLIST'], 'if': ['STMT', 'STMTLIST'], '{': ['STMT', 'STMTLIST'], ';': ['STMT', 'STMTLIST'], '}': [], '$': []},
-    'EXPR': {'id': ['NUMEXPR'], 'num': ['NUMEXPR'], '(': ['NUMEXPR']},
-    'NUMEXPR': {'id': ['TERM'], 'num': ['TERM'], '(': ['TERM']},
-    'TERM': {'id': ['FACTOR'], 'num': ['FACTOR'], '(': ['FACTOR']},
-    'FACTOR': {'id': ['id'], 'num': ['num'], '(': ['(', 'NUMEXPR', ')']}
+    'EXPR': {'id': ['NUMEXPR', 'EXPRTAIL'], 'num': ['NUMEXPR', 'EXPRTAIL'], '(': ['NUMEXPR', 'EXPRTAIL'], ';': [';', '}']},
+    'EXPRTAIL': {'+': ['+', 'NUMEXPR', 'EXPRTAIL'], '-': ['-', 'NUMEXPR', 'EXPRTAIL'], '==': ['==', 'NUMEXPR', 'EXPRTAIL'], '$': [], ')': [], ';': []},
+    'NUMEXPR': {'id': ['TERM', 'NUMEXPRTAIL'], 'num': ['TERM', 'NUMEXPRTAIL'], '(': ['TERM', 'NUMEXPRTAIL']},
+    'NUMEXPRTAIL': {'+': ['+', 'TERM', 'NUMEXPRTAIL'], '-': ['-', 'TERM', 'NUMEXPRTAIL'], '$': [], ')': [], ';': [], '==': ['==', 'TERM', 'NUMEXPRTAIL']},
+    'TERM': {'id': ['FACTOR', 'TERMTAIL'], 'num': ['FACTOR', 'TERMTAIL'], '(': ['FACTOR', 'TERMTAIL']},
+    'TERMTAIL': {'*': ['*', 'FACTOR', 'TERMTAIL'], '$': [], ')': [], ';': [], '+': [], '-': [], '==': []},
+    'FACTOR': {'id': ['id'], 'num': ['num'], '(': ['(', 'EXPR', ')']}
 }
 
 def parse(tokens):
@@ -63,7 +68,9 @@ exemplos = [
     ["$","def", "id", "(", "int", "id", ")", "{", "int", "id", ";", "}"], # valido
     ["$","id", "=", "num", ";"], # valido
     ["$","print", "num", ";"], # valido
+    ["$", "def", "id", "(", "int", "id", ",", "int", "id", ")", "{", "id", "=", "id", "+", "id", ";", "id", "=", "id", "*", "id", ";", "return", "id", ";", "}"], #valido
 ]
+
 
 for tokens in exemplos:
     result = parse(tokens)
