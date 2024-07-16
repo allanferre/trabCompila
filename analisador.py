5f8026a2447d183e6006f1beb8abedb6b532712a
gabrielguglik <gabrielguglielmik@gmail.com>
2024-07-16 10:00:33 -0300

    tabela corrigida e casos de teste passando

--- analisador.py
+++ analisador.py
@@ -3,14 +3,15 @@ parse_table = {
     'MAIN': {'def': ['FLIST'], 'int': ['STMT'], 'id': ['STMT'], 'print': ['STMT'], 'return': ['STMT'], 'if': ['STMT'], '{': ['STMT'], ';': ['STMT'], '$': []},
     'FLIST': {'def': ['FDEF', 'FLIST'], '$': []},
     'FDEF': {'def': ['def', 'id', '(', 'PARLIST', ')', '{', 'STMTLIST', '}']},
-    'PARLIST': {'int': ['int', 'id', ',', 'PARLIST'], ')': [], '$': []},
+    'PARLIST': {'int': ['int', 'id', 'PARLIST'], ',': [',', 'PARLIST'], ')': [], '$': []},
     'STMT': {'int': ['int', 'id', ';'], 'id': ['ATRIBST', ';'], 'print': ['PRINTST', ';'], 'return': ['RETURNST', ';'], 'if': ['IFSTMT'], '{': ['{', 'STMTLIST', '}'], ';': [';']},
     'ATRIBST': {'id': ['id', '=', 'EXPR']},
     'FCALL': {'id': ['id', '(', 'PARLISTCALL', ')']},
     'PARLISTCALL': {'id': ['id', ',', 'PARLISTCALL'], ')': [], '$': []},
     'PRINTST': {'print': ['print', 'EXPR']},
     'RETURNST': {'return': ['return']},
-    'IFSTMT': {'if': ['if', '(', 'EXPR', ')', 'STMT', 'else', 'STMT'], 'else': ['if', '(', 'EXPR', ')', 'STMT']},
+    'IFSTMT': {'if': ['if', '(', 'EXPR', ')', 'STMT', 'ELSEPART']},
+    'ELSEPART': {'else': ['else', 'STMT'], '': [], '$': []}, # verificar com o professor
     'STMTLIST': {'int': ['STMT', 'STMTLIST'], 'id': ['STMT', 'STMTLIST'], 'print': ['STMT', 'STMTLIST'], 'return': ['STMT', 'STMTLIST'], 'if': ['STMT', 'STMTLIST'], '{': ['STMT', 'STMTLIST'], ';': ['STMT', 'STMTLIST'], '}': [], '$': []},
     'EXPR': {'id': ['NUMEXPR'], 'num': ['NUMEXPR'], '(': ['NUMEXPR']},
     'NUMEXPR': {'id': ['TERM'], 'num': ['TERM'], '(': ['TERM']},
@@ -49,21 +50,21 @@ def parse(tokens):
         return False
 
 # Exemplos de tokens
-examplos = [
-    ["$","def", "(", "int", "id", ")", "{", "int", "id", "=", "num", ";", "}", ""],   
-    ["$","def", "(", "int", "id", ")", "{", "", "}", ""],  
-    ["$","int", "id", "=", "num", ";", ""],                                       
-    ["$","print", "id", ";"],                                                  
-    ["$","def", "(", "int", ")", "{", "int", "id", "=", "num", ";", "}", ""],     
-    ["$","int", "id", "num", ";", ""],                                             
-    ["$","num"],                                             
-    ["$","return", "id", ";", ""],                                                 
-    ["$","def", "(", "int", "id", ")", "{", "id", "=", "num", ";", "}"],
-    ["$","def", "id", "(", "int", "id", ")", "{", "int", "id", ";", "}"],
-    ["$","id", "=", "num", ";"],
-    ["$","print", "num", ";"],
+exemplos = [
+    ["$","def", "(", "int", "id", ")", "{", "int", "id", "=", "num", ";", "}"], # invalido
+    ["$","def", "(", "int", "id", ")", "{", "", "}"], # invalido 
+    ["$","int", "id", "=", "num", ";"], # invalido                                       
+    ["$","print", "id", ";"], # valido                                                  
+    ["$","def", "(", "int", ")", "{", "int", "id", "=", "num", ";", "}"], # invalido     
+    ["$","int", "id", "num", ";"], # invalido                                             
+    ["$","num"], # invalido                                             
+    ["$","return", ";"], # valido - verificar se pode ter variável depois do return de acordo com a gramática                                                 
+    ["$","def", "(", "int", "id", ")", "{", "id", "=", "num", ";", "}"], # invalido
+    ["$","def", "id", "(", "int", "id", ")", "{", "int", "id", ";", "}"], # valido
+    ["$","id", "=", "num", ";"], # valido
+    ["$","print", "num", ";"], # valido
 ]
 
-for tokens in examplos:
+for tokens in exemplos:
     result = parse(tokens)
     print(f"A lista de tokens {tokens} é válida de acordo com a gramática: {result}")
